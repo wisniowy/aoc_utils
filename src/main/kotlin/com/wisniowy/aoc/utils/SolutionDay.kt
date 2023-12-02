4905ad82b85d5fa51aea457f6f809652ccc9fc47
michal-wisniewski <michal.w.wisniewski@gmail.com>
2023-12-02 15:04:32 +0100

    added handling of part separator in input file

--- src/main/kotlin/com/wisniowy/aoc/utils/SolutionDay.kt
+++ src/main/kotlin/com/wisniowy/aoc/utils/SolutionDay.kt
@@ -14,11 +14,11 @@ abstract class SolutionDay(val dayNumber: Int) {
 
     abstract fun partTwo() : Any
 
-    protected fun inputAsString() : String {
-        return FileReader.inputAsString(file)
+    protected fun inputAsString(inputPart: FileReader.InputPart = FileReader.InputPart.PART_ONE) : String {
+        return FileReader.inputAsString(file, inputPart)
     }
 
-    protected fun inputAsStringList(): List<String> {
-        return FileReader.inputAsStringList(file)
+    protected fun inputAsStringList(inputPart: FileReader.InputPart = FileReader.InputPart.PART_ONE): List<String> {
+        return FileReader.inputAsStringList(file, inputPart)
     }
 }
