b7a91c4367443b09e06d18f4d6da3a675b846329
michal-wisniewski <michal.w.wisniewski@gmail.com>
2023-12-02 15:03:26 +0100

    added handling of part separator in input file

--- src/main/kotlin/com/wisniowy/aoc/utils/FileReader.kt
+++ src/main/kotlin/com/wisniowy/aoc/utils/FileReader.kt
@@ -5,12 +5,20 @@ import java.io.FileNotFoundException
 
 object FileReader {
 
-    fun inputAsString(file: File) : String {
-        return file.readText()
+    const val PART_SEPARATOR = "### PART SEPARATOR ###\n"
+    enum class InputPart(val idx: Int) { PART_ONE(0), PART_TWO(1)}
+
+    fun inputAsString(file: File, inputPart: InputPart = InputPart.PART_ONE) : String {
+        val inputParts = file.readText().split(PART_SEPARATOR)
+
+        return if (inputParts.size == 1) inputParts[InputPart.PART_ONE.idx].trim() else inputParts[inputPart.idx].trim()
     }
 
-    fun inputAsStringList(file: File) : List<String> {
-        return file.readLines()
+    fun inputAsStringList(file: File, inputPart: InputPart = InputPart.PART_ONE) : List<String> {
+        val inputParts = file.readText().split(PART_SEPARATOR)
+
+        return if (inputParts.size == 1) inputParts[InputPart.PART_ONE.idx].trim().lines()
+            else inputParts[inputPart.idx].trim().lines()
     }
 
     internal fun loadFileFromResources(dayNumber: Int) : File {
