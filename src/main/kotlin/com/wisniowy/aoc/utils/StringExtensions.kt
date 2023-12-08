a2501a1a01d81b287b741f75548057637273c29e
michal-wisniewski <michal.w.wisniewski@gmail.com>
2023-12-08 23:53:00 +0100

    fix StringExtensions.kt

--- src/main/kotlin/com/wisniowy/aoc/utils/StringExtensions.kt
+++ src/main/kotlin/com/wisniowy/aoc/utils/StringExtensions.kt
@@ -1,9 +1,9 @@
 package com.wisniowy.aoc.utils
 
-fun String.strToIntList(str: String): List<Int> {
-    return "\\d+".toRegex().findAll(str).map { it.value.toInt() }.toList()
+fun String.toIntList(): List<Int> {
+    return "\\d+".toRegex().findAll(this).map { it.value.toInt() }.toList()
 }
 
-fun String.strToLongList(str: String): List<Long> {
-    return "\\d+".toRegex().findAll(str).map { it.value.toLong() }.toList()
+fun String.toLongList(): List<Long> {
+    return "\\d+".toRegex().findAll(this).map { it.value.toLong() }.toList()
 }
