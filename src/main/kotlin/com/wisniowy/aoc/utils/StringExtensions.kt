831e206f563964b05c77ac41193448cc9a2cd2c7
michal-wisniewski <michal.w.wisniewski@gmail.com>
2023-12-10 13:13:02 +0100

    update toIntList and toLongList

--- src/main/kotlin/com/wisniowy/aoc/utils/StringExtensions.kt
+++ src/main/kotlin/com/wisniowy/aoc/utils/StringExtensions.kt
@@ -1,9 +1,9 @@
 package com.wisniowy.aoc.utils
 
 fun String.toIntList(): List<Int> {
-    return "\\d+".toRegex().findAll(this).map { it.value.toInt() }.toList()
+    return "-?\\d+".toRegex().findAll(this).map { it.value.toInt() }.toList()
 }
 
 fun String.toLongList(): List<Long> {
-    return "\\d+".toRegex().findAll(this).map { it.value.toLong() }.toList()
+    return "-?\\d+".toRegex().findAll(this).map { it.value.toLong() }.toList()
 }
